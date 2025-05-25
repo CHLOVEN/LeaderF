522401723bd167913c575cf100d6985e465bf9f6
Chloven Price <45620694+CHLOVEN@users.noreply.github.com>
2025-05-25 18:41:54 +0800

    change title for hint

--- autoload/leaderf/python/leaderf/manager.py
+++ autoload/leaderf/python/leaderf/manager.py
@@ -2719,6 +2719,8 @@ class Manager(object):
             self._getInstance().clearBuffer()
 
     def startExplorer(self, win_pos, *args, **kwargs):
+        vim.options['title'] = True
+        vim.options['titlestring'] = "LeaderF_mode"
         arguments_dict = kwargs.get("arguments", {})
         if "--recall" in arguments_dict:
             self._arguments.update(arguments_dict)
