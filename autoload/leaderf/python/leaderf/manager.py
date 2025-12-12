99f982cb35f514708495390eb92ab2ee9cce08b0
Chloven Price <chlovenp@gmail.com>
2025-12-12 10:03:11 +0800

    - back to prioritied as another group

--- autoload/leaderf/python/leaderf/manager.py
+++ autoload/leaderf/python/leaderf/manager.py
@@ -1705,7 +1705,7 @@ class Manager(object):
                     digest = mru.normalizeForPriority(digest)
                     prio = priority_map.get(digest, 0)
                     if prio > 0:
-                         weight += prio
+                         weight += 10000
                     new_result.append((weight, line))
                 result = new_result
 
